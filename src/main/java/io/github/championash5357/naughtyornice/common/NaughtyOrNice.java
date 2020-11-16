63232ad0fb198c3fbd78fcf263a40f7e5530c614
ChampionAsh5357 <championash5357@gmail.com>
2020-11-16 10:05:27 -0500

    Initial commit

---
+++ src/main/java/io/github/championash5357/naughtyornice/common/NaughtyOrNice.java
@@ -0,0 +1,9 @@
+package io.github.championash5357.naughtyornice.common;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(NaughtyOrNice.ID)
+public class NaughtyOrNice {
+
+	public static final String ID = "naughtyornice";
+}
